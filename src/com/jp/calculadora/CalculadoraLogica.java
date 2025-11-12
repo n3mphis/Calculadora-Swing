2d77d76dbce778b4f0f178333b7a3162c9993710
n3mphis <66238416+n3mphis@users.noreply.github.com>
2025-11-12 12:29:17 -0300

    Creado el metodo para hacer los calculos dentro de CalculadoraLogica

--- src/com/jp/calculadora/CalculadoraLogica.java
+++ src/com/jp/calculadora/CalculadoraLogica.java
@@ -1,4 +1,19 @@
 package com.jp.calculadora;
 
 public class CalculadoraLogica {
+
+    public double calcular(double num1, String operador, double num2) {
+        return switch (operador) {
+            case "+" -> num1 + num2;
+            case "-" -> num1 - num2;
+            case "x" -> num1 * num2;
+            case "÷" -> {
+                if (num2 == 0) {
+                    throw new ArithmeticException("División por cero");
+                }
+                yield num1 / num2;
+            }
+            default -> throw new IllegalArgumentException("Operador no válido: " + operador);
+        };
+    }
 }
